38d33f38276bb7543b68c8e7a9bbb1dadf013ff9
Denis Paul <denis.paul@softvision.com>
2021-12-23 15:25:30 +0200

    added filter for product categories and maded UI changes

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,6 +1,10 @@
 class ShopsController < ApplicationController
   def index
-    @products = Product.all
+    if params[:filter]
+      @products = Product.where(category:params[:filter])
+    else
+      @products = Product.all
+    end
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :products
   resources :shops, only:[:index, :show]
-  # root 'home#index'
+  root 'home#index'
   # root 'products#index'
-  root 'shops#index'
+  # root 'shops#index'
 end
