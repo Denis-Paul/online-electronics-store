aadc2032345c7ab621584151a6308c3dc0ae47ec
Denis Paul <denis.paul@softvision.com>
2021-12-28 09:50:42 +0200

    prevent sql injection

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ApplicationRecord
     # belongs_to :category
 
-    scope :search_by_name, ->(query) { where("lower(name) LIKE ?", "%#{query.downcase}%") }
-    scope :search_by_description, ->(query) { where("lower(description) LIKE ?", "%#{query.downcase}%") }
+    scope :search_by_name, ->(query) { where(["lower(name) LIKE ?", "%#{query.downcase}%"]) }
+    scope :search_by_description, ->(query) { where(["lower(description) LIKE ?", "%#{query.downcase}%"]) }
 
     # def self.search(query)
     #     where("lower(name) LIKE ? OR lower(description) LIKE ?", "%#{query.downcase}%", "%#{query.downcase}%") 
